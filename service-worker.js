7d63fcb46d5055934157978aab6de83046628dc8
WilsonPhoo <wilson@policypal.com>
2019-04-25 01:26:42 +0800

    - Update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.534e86511d6a0848b1b1f56d37930a94.js"
+  "/precache-manifest.4a59d501028445792290b4bb86bf1d63.js"
 );
 
 workbox.clientsClaim();
